55913465f0caa21a7b2c61f6cd81a676e4d7777c
LSChyi <alan81920@gmail.com>
2017-02-11 21:32:04 +0800

    update ajax base url

--- update_ui/static/app/config.js
+++ update_ui/static/app/config.js
@@ -1,5 +1,5 @@
 angular.module("Config", []);
 Config = angular.module('Config');
 
-Config.constant("API_BASE", "http://140.112.42.124:8000/");
+Config.constant("API_BASE", "/");
 //Config.constant("API_BASE", "http://localhost:8080/");
